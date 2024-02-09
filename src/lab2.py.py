dd7f53bccf5c89e4b64990c2dd4065f64141d0c4
Abraham Muldrow <abe@muldrow.net>
2024-02-08 16:44:15 -0800

    Update lab2.py.py

--- src/lab2.py.py
+++ src/lab2.py.py
@@ -15,15 +15,14 @@ t_4 = pyb.Timer(4, freq = 1000)
 # pyb.Timer.ENC_AB(t_4)  # start the timer from the timer value
 # tch1 = t.channel(pyb.Timer.A, pin=pinPB6)  # create the timer channels for each motor pin
 # tch2 = t.channel(pyb.Timer.B, pin=pinPB7)
-enc_channel_1 = t_4.channel(pin=pinPB6, pyb.Timer.ENC_AB)
-enc_channel_2 = t_4.channel(pin=pinPB7, pyb.Timer.ENC_AB)
-
+enc_channel_1 = t_4.channel(1, pyb.Timer.ENC_AB, pin = pinPB6)
+enc_channel_2 = t_4.channel(2, pyb.Timer.ENC_AB, pin = pinPB7)
 
 
 while True:
     try:
         value = t_4.counter()
         print(value)
-        time.sleep(2)
+        time.sleep(0.25)
     except KeyboardInterrupt:
         break
