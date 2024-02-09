4b9c8c59a3b551600ee7860d2c36a59ea34d5409
Abraham Muldrow <abe@muldrow.net>
2024-02-08 16:28:29 -0800

    Update lab2.py.py

---
+++ src/lab2.py.py
@@ -0,0 +1,27 @@
+import micropython
+import time
+import pyb
+
+
+pinPB6 = pyb.Pin(pyb.Pin.board.PB6, pyb.Pin.IN)
+pinPB7 = pyb.Pin(pyb.Pin.board.PB7, pyb.Pin.IN)
+
+
+# adcpin_6 = pyb.ADC(pinPB6)
+# adcpin_7 = pyb.ADC(pinPB7)
+
+t_4 = pyb.Timer(4, freq = 1000)
+
+# pyb.Timer.ENC_AB(t_4)  # start the timer from the timer value
+# tch1 = t.channel(pyb.Timer.A, pin=pinPB6)  # create the timer channels for each motor pin
+# tch2 = t.channel(pyb.Timer.B, pin=pinPB7)
+enc_channel = t_4.channel(1, pyb.Timer.ENC_AB)
+
+
+while True:
+    try:
+        value = t_4.counter()
+        print(value)
+        time.sleep(2)
+    except KeyboardInterrupt:
+        break
